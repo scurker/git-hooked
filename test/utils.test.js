38e615a283fa689754602715154bb9dd9152967a
Jason <jason@scurker.com>
2015-04-12 00:00:55 -0500

    hooks

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,27 +6,25 @@ afterEach(mock.restore);
 
 describe('File utils', function() {
 
+  beforeEach(function() {
+    mock({ '.git/hooks': {
+      'pre-commit': 'querty'
+    }});
+  });
+
   it('should check that a directory exists', function() {
-    mock({ '.git/hooks': {} });
     assert.equal(true, utils.dirExists('.git/hooks'));
   });
 
   it('should check that a directory does not exist', function() {
-    mock({ '.git/hooks': {} });
     assert.equal(false, utils.dirExists('.git/hooked'));
   });
 
   it('should check that a file exists', function() {
-    mock({ '.git/hooks': {
-      'pre-commit': 'qwerty'
-    }});
     assert.equal(true, utils.fileExists('.git/hooks/pre-commit'));
   });
 
   it('should check that a file does not exist', function() {
-    mock({ '.git/hooks': {
-      'pre-commit': 'querty'
-    }});
     assert.equal(false, utils.fileExists('.git/hooks/post-commit'));
   });
 
